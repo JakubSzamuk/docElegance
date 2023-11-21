510bf8450d83780d4ee55e1b141974195a80419e
Jakub Szamuk <jakubszamuk@BlueBerry-Pi.broadband>
2023-11-21 22:29:23 +0000

    fixed puppeteer, Need to fix authentication for it

---
+++ .puppeteerrc.cjs
@@ -0,0 +1,4 @@
+module.exports = {
+  // Changes the cache location for Puppeteer.
+  cacheDirectory: '/usr/src/.cache/puppeteer',
+};
--- app/api/autosaver/route.ts
+++ app/api/autosaver/route.ts
@@ -3,10 +3,21 @@ import { PrismaClient } from "@prisma/client";
 import { useSession } from "next-auth/react";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/route";
-
+import puppeteer from "puppeteer";
 
 const prisma = new PrismaClient()
 export const revalidate = 0
+
+const getThumbnail = async (body: any) => {
+  const browser = await puppeteer.launch({
+    executablePath: 'google-chrome-stable',
+    args: ['--no-sandbox'],
+  });
+  const page = await browser.newPage();
+  await page.goto(`${process.env.NEXTAUTH_URL}doc/${body.id}`);
+
+  await page.screenshot({ path: `./public/thumbnails/${body.id}.png` });
+}
 
 export async function POST(req: NextRequest, res: NextResponse) {
   const body = await req.json();
@@ -18,7 +29,9 @@ export async function POST(req: NextRequest, res: NextResponse) {
         email: session?.user?.email
       }
     }))?.id
-  
+
+    getThumbnail(body)
+
     try {
       if (body.id == null) {
         return NextResponse.json({ status: false })
@@ -37,9 +50,9 @@ export async function POST(req: NextRequest, res: NextResponse) {
       }
     } catch (err) {
       return NextResponse.json(`error, ${err}`)
-    } 
+    }
   } else {
     return NextResponse.json("You are not signed in")
   }
 }
-  
+
